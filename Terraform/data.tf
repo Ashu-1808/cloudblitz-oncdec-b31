bc16322d427a3709a705924405f543fe73a56585
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2026-01-08 20:39:51 +0530

    Create data.tf

---
+++ Terraform/data.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20251022"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "pankaj"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
